809703b34934ac379d9a238dbb58ecd1de264a02
lordish <gitonga403@gmail.com>
2023-06-19 06:22:15 +0300

    final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 50)
+Student.create(first_name: 'Morris', last_name: 'Gitonga', grade: 80)
